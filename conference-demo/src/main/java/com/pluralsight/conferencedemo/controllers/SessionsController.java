17c6704d70ef154754af96f9e41cd8c92c82d2c6
jlauth <jlauth@LHTU05CG021CYD9.ms.ds.uhc.com>
2020-09-03 10:42:52 -0500

    first commit of branch

--- conference-demo/src/main/java/com/pluralsight/conferencedemo/controllers/SessionsController.java
+++ conference-demo/src/main/java/com/pluralsight/conferencedemo/controllers/SessionsController.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+// Adding Comments
 @RestController
 @RequestMapping("/api/v1/sessions")
 public class SessionsController {
